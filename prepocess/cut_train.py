2786ce3e42d910a6c12732621687b1382ad5561e
Yunfan Hu <1033718596@qq.com>
2021-03-03 17:23:04 +0800

    spare one day to be test

--- prepocess/cut_train.py
+++ prepocess/cut_train.py
@@ -4,7 +4,7 @@ import json
 
 train = pd.read_csv('./data/train/behaviors.tsv', sep="\t", encoding="utf-8", names=["id", "uid", "time", "hist", "imp"])
 train['date'] = train['time'].apply(lambda x: x.split()[0])
-his_day = train[train['date'] != '11/14/2019']
+his_day = train[(train['date'] != '11/14/2019') & (train['date'] != '11/13/2019')]
 target_day = train[train['date'] == '11/14/2019']
 
 his_day.drop(columns=['date']).to_csv('./data/train/his_behaviors.tsv', sep="\t", encoding="utf-8", header=None, index=None)
