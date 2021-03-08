90e2f1888c86c5a4576e7a733e9a334ddac35dad
Yunfan Hu <1033718596@qq.com>
2021-03-08 09:40:19 +0800

    change target file

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ python -m prepocess.build_dicts
 python -m prepocess.find_neighbors
 python -m prepocess.build_train --processes=10
 python -m prepocess.resplit --filenum 10 --processes 4
-python -m prepocess.build_valid --processes=10 --fsamples=dev/behaviors.tsv
-CUDA_VISIBLE_DEVICES=0,1,2,3 python training.py --gpus=4 --epoch=10 --filenum=1
+python -m prepocess.build_valid --processes=10 --fsamples=dev/target_behaviors.tsv
+CUDA_VISIBLE_DEVICES=4,5,6,7 python training.py --gpus=4 --epoch=10 --filenum=1
 CUDA_VISIBLE_DEVICES=0,1,2,3 python validate.py --gpus=4 --epoch=10 --filenum=10
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 python deers_train.py --gpus=4 --epoch=10 --filenum=1
