3a3f35fa32e390167aeed3f5ca95af035c0c41a8
Yunfan Hu <1033718596@qq.com>
2021-03-29 16:22:51 +0800

    resplit

---
+++ adressa_prepocess/resplit.py
@@ -0,0 +1,29 @@
+import numpy as np
+import argparse
+import json
+import math
+import os
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--fsamples", default="adressa/raw/dev", type=str,
+                    help="Path of the training samples file.")
+parser.add_argument("--split", default=10, type=int,
+                    help="Processes number")
+parser.add_argument('--filenum', type=int, default=10)
+
+cfg = parser.parse_args()
+
+data_list = []
+
+for i in range(cfg.filenum):
+    file_name = "{}-{}.npy".format(cfg.fsamples, i)
+    data_list.append(np.load(file_name))
+    os.remove(file_name)
+
+datanp = np.concatenate(data_list, axis=0)
+
+sub_len = math.ceil(len(datanp) / cfg.split)
+
+np.save("{}-{}.npy".format(cfg.fsamples, 0), datanp[:sub_len])
+np.save("{}-{}.npy".format(cfg.fsamples, 0), datanp[sub_len:])
